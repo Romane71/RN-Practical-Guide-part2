eb04b7115a0a4f6e9f7937aca3a2e4f276fcf0c4
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-07-22 17:40:10 +0100

    Game-first commit

---
+++ RN-Game/screens/StartGameScreen.js
@@ -0,0 +1,13 @@
+import { TextInput , View} from "react-native";
+import { PrimaryButton } from "../components/PrimaryButton";
+
+
+export function StartGameScreen() {
+
+    
+return <View>
+    <TextInput />
+    <PrimaryButton>RESET</PrimaryButton>
+    <PrimaryButton>CONFIRM</PrimaryButton>
+</View>
+}
