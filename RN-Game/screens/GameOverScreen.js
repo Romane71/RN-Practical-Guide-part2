066392d0a62b05e7aa7b24467755d30097d8e6de
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-07-23 21:41:43 +0100

    add changes

--- RN-Game/screens/GameOverScreen.js
+++ RN-Game/screens/GameOverScreen.js
@@ -1,6 +1,52 @@
-import { Text } from "react-native";
+import { Image, View, StyleSheet, Text} from "react-native";
+import { Title } from "../components/ui/Title";
+
 
 
 export function GameOverScreen() {
-    return <Text>Game is Over</Text>
-}
+    return ( <View style={styles.rootContainer}>
+        <Title>GAME OVER</Title>
+        <View style={styles.imageContainer}>
+        <Image 
+        style={styles.image} 
+        source={require('../assets/images/success.png')}/>
+        </View>
+        <Text style={styles.summaryText}>Your phone needed
+         <Text style={styles.highlight}>X</Text> rounds to guess the number
+      <Text style={styles.highlight}>Y</Text>
+      </Text>
+    </View>
+)}
+
+const styles = StyleSheet.create({
+
+    rootContainer: {
+        flex: 1,
+        padding: 24,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+ 
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'khaki',
+        overflow: 'hidden',
+        margin: 36
+    },
+
+    image: {
+        width: '100%',
+        height: '100%'
+    },
+
+    summaryText: {
+
+    }, 
+    highlight: {
+
+    }
+
+})
