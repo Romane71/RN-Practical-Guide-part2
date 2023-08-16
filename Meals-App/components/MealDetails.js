acb557a7cc0b5b6c956018cd8e28ffc503b62631
Romane71 <120040498+Romane71@users.noreply.github.com>
2023-08-16 01:23:36 +0100

    finishing the meal detail screen + styling

--- Meals-App/components/MealDetails.js
+++ Meals-App/components/MealDetails.js
@@ -1,12 +1,14 @@
 import { View, Text, StyleSheet} from 'react-native'
 
-export function MealDetails({duration, complexity, affordability}) {
-    return      <View style={styles.details}>
-    <Text style={styles.detailItem}>{duration}m</Text>
-    <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-    <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+export function MealDetails({duration, complexity, affordability, style, textStyle}) {
+    return    ( 
+      <View style={[styles.details, style]}>
+
+    <Text style={[styles.detailItem, textStyle]}>{duration}m</Text>
+    <Text style={[styles.detailItem, textStyle]}>{complexity.toUpperCase()}</Text>
+    <Text style={[styles.detailItem, textStyle]}>{affordability.toUpperCase()}</Text>
   </View>
-}
+)}
 
 const styles = StyleSheet.create({
   details: {
